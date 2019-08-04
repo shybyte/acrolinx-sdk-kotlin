9290606e338c8ccd298e1e0f52930105d4397918
shybyte <ein@volloeko.de>
2019-08-04 13:26:51 +0200

    Use mockk

--- src/test/kotlin/integration/SignInInteractiveTest.kt
+++ src/test/kotlin/integration/SignInInteractiveTest.kt
@@ -1,13 +1,17 @@
 package integration
 
 import assertk.assertThat
-import assertk.assertions.*
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotEmpty
+import assertk.assertions.isNotNull
 import com.acrolinx.client.sdk.InteractiveCallback
-import com.acrolinx.client.sdk.exceptions.SignInException
 import integration.common.ACROLINX_API_TOKEN
 import integration.common.ACROLINX_API_USERNAME
 import integration.common.ACROLINX_URL
 import integration.common.BaseIntegrationTest
+import io.mockk.Called
+import io.mockk.mockk
+import io.mockk.verify
 import java.util.concurrent.ExecutionException
 import kotlin.test.BeforeTest
 import kotlin.test.Test
@@ -15,6 +19,8 @@ import kotlin.test.fail
 
 
 class SignInInteractiveTest : BaseIntegrationTest() {
+    private val interactiveCallback = mockk<InteractiveCallback>()
+
     @BeforeTest
     fun beforeTest() {
         org.junit.Assume.assumeTrue(ACROLINX_API_USERNAME != null && ACROLINX_API_TOKEN !== null)
@@ -22,29 +28,21 @@ class SignInInteractiveTest : BaseIntegrationTest() {
 
     @Test
     fun testSignInWithPolling() {
-        var interactiveUrl: String? = null
         try {
-            val signInSuccess = acrolinxEndpoint.signInInteractive(object : InteractiveCallback {
-                override fun onInteractiveUrl(url: String) {
-                    interactiveUrl = url
-                }
-            }, timeoutMs = 10).get()
+            val signInSuccess = acrolinxEndpoint.signInInteractive(interactiveCallback, timeoutMs = 10).get()
             fail("Should throw exception but was $signInSuccess")
         } catch (e: ExecutionException) {
-            assertThat(interactiveUrl!!).startsWith(ACROLINX_URL!!)
+            verify { interactiveCallback.onInteractiveUrl(match { it.startsWith(ACROLINX_URL!!) }) }
         }
     }
 
     @Test
     fun testSignInWithPollingWithValidAuthToken() {
-        var interactiveUrl: String? = null
-        val signInSuccess = acrolinxEndpoint.signInInteractive(object : InteractiveCallback {
-            override fun onInteractiveUrl(url: String) {
-                interactiveUrl = url
-            }
-        }, timeoutMs = 10, accessToken = ACROLINX_API_TOKEN).get()
+        val signInSuccess =
+            acrolinxEndpoint.signInInteractive(interactiveCallback, timeoutMs = 10, accessToken = ACROLINX_API_TOKEN)
+                .get()
 
-        assertThat(interactiveUrl).isNull()
+        verify { interactiveCallback wasNot Called }
 
         assertThat(signInSuccess.user.username).isEqualTo(ACROLINX_API_USERNAME)
         assertThat(signInSuccess.accessToken.token).isNotEmpty()
