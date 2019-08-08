bbcfbc5143a71e9e7bbae9cbd5f62c0375885134
shybyte <ein@volloeko.de>
2019-08-08 08:54:16 +0200

    more properties in CheckingCapabilities

--- src/main/kotlin/platform/CheckingCapabilities.kt
+++ src/main/kotlin/platform/CheckingCapabilities.kt
@@ -1,8 +1,19 @@
 package com.acrolinx.client.sdk.platform
 
+import com.acrolinx.client.sdk.check.CheckType
+import com.acrolinx.client.sdk.check.ContentEncoding
+import com.acrolinx.client.sdk.check.ReportType
 import kotlinx.serialization.Serializable
 
 @Serializable
 class CheckingCapabilities(
-    val guidanceProfiles: List<GuidanceProfile>
-)
+    val guidanceProfiles: List<GuidanceProfile>,
+    val contentFormats: List<ContentFormat>,
+    val contentEncodings: List<ContentEncoding>,
+    val referencePattern: String,
+    val checkTypes: List<CheckType>,
+    val reportTypes: List<ReportType>
+    )
+
+@Serializable
+class ContentFormat(val id: String, val displayName: String)
